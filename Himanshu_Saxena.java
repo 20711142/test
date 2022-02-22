ea5d4d87372772af389ca6a522ca3ad6e4f01481
HIMANSHU SAXENA 20711142 <86280220+20711142@users.noreply.github.com>
2022-02-22 17:13:44 +0530

    Create Himanshu_Saxena.java

---
+++ Himanshu_Saxena.java
@@ -0,0 +1,10 @@
+public class concat {
+
+    public static void main(String args[])
+    {
+       String str1="Himanshu "; 
+        String str2=" Saxena";
+        System.out.println("Concatenated String is ");
+        System.out.println("Hello "+str1.concat(str2));
+    }
+}
